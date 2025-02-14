11928b3595fb6af8f3911544da0f38d3d388986d
ElinaSav <kaytukova_01@mail.ru>
2025-02-14 17:45:59 +0300

    fix(main):Исправлены ошибки, начало работы над адаптивом

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
 (function(){
     const closeItem = document.querySelector(".popup__close__subscrib");
     const popup = document.querySelector(".popup__screen");
-    console.log(closeItem)
     closeItem.addEventListener("click", () =>{
-        console.log(closeItem)
         popup.classList.add("popup__screen_active");
     });
   }())
